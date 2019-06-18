22a726159e0f88068c85fa35ff2522a8d9f2bb90
范丽娜 <fanln@mail.taiji.com.cn>
2019-06-18 21:00:47 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,86 +1,77 @@
 <template>
-<div>
+<div class="login-container">
     <h3>系统登录</h3>
     <el-form :model="loginForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
         <el-form-item label="用户名" prop="username">
             <span class="svg-container">
-            <svg-icon icon-class="user" />
-          </span>
-            <el-input
-            ref="username"
-            v-model="loginForm.username"
-            placeholder="Username"
-            name="username"
-            type="text"
-            tabindex="1"
-            autocomplete="on" />
+          <svg-icon icon-class="user" />
+        </span>
+            <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text" tabindex="1" autocomplete="on" />
         </el-form-item>
-        <el-form-item label="密码" prop="password">
-            <span class="svg-container">
+        <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+            <el-form-item label="密码" prop="password">
+                <span class="svg-container">
             <svg-icon icon-class="password" />
           </span>
-            <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="Password"
-            name="password"
-            tabindex="2"
-            autocomplete="on"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
-            @keyup.enter.native="handleLogin" />
-        </el-form-item>
+                <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="Password" name="password" tabindex="2" autocomplete="on" @keyup.native="checkCapslock" @blur="capsTooltip = false" @keyup.enter.native="handleLogin" />
+                <span class="show-pwd" @click="showPwd">
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          </span>
+            </el-form-item>
+        </el-tooltip>
         <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-            <el-button @click="resetForm('ruleForm')">重置</el-button>
+            <el-button type="primary" @click="login('ruleForm')">登录</el-button>
+            <el-button @click="reset('ruleForm')">重置</el-button>
         </el-form-item>
     </el-form>
-
 </div>
 </template>
 
 <script>
-// import { validUsername } from '@/utils/validate'
 export default {
   data () {
-    // const validateUsername = (rule, value, callback) => {
-    //   if (!validUsername(value)) {
-    //     callback(new Error('Please enter the correct user name'))
-    //   } else {
-    //     callback()
-    //   }
-    // }
-    // const validatePassword = (rule, value, callback) => {
-    //   if (value.length < 6) {
-    //     callback(new Error('The password can not be less than 6 digits'))
-    //   } else {
-    //     callback()
-    //   }
-    // }
     return {
       loginForm: {
         username: '',
         password: ''
       },
-      rules: {
-        username: [{
-          required: true,
-          trigger: 'blur'
-          // validator: validateUsername
-        }],
-        password: [{
-          required: true,
-          trigger: 'blur'
-          // validator: validatePassword
-        }]
-      }
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', message: '请输入用户名' }],
+        password: [{ required: true, trigger: 'blur', validator: '请输入要密码' }]
+      },
+      passwordType: 'password',
+      capsTooltip: false,
+      loading: false
+    }
+  },
+  mounted () {
+    if (this.loginForm.username === '') {
+      this.$refs.username.focus()
+    } else if (this.loginForm.password === '') {
+      this.$refs.password.focus()
+    }
+    this.login()
+  },
+  methods: {
+    // 登录
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    // 重置
+    reset (formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
---
+++ src/views/login/index2.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login-container">
+    <h3>系统登录</h3>
+    <el-form :model="loginForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text" tabindex="1" autocomplete="on" />
+      </el-form-item>
+      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+        <el-form-item label="密码" prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password" />
+          </span>
+          <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="Password" name="password" tabindex="2" autocomplete="on" @keyup.native="checkCapslock" @blur="capsTooltip = false" @keyup.enter.native="handleLogin" />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          </span>
+        </el-form-item>
+      </el-tooltip>
+      <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ required: true, trigger: 'blur', message:'请输入用户名！' }],
+        password: [
+          { required: true, trigger: 'blur', message:'请输入密码！'  },
+          { min: 6, message: '密码最少6个字符！', trigger: 'blur' }
+        ]
+      },
+      passwordType: 'password',
+      capsTooltip: false,
+      loading: false,
+    }
+  },
+  watch: {
+  },
+  mounted() {
+    if (this.loginForm.username === '') {
+      this.$refs.username.focus()
+    } else if (this.loginForm.password === '') {
+      this.$refs.password.focus()
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.login-container {
+  clolor:erer
+}
+</style>
